ecb50356ac222d71b945a99852ef1dc3391ca238
Brandon <brandon.cole@bison.howard.edu>
2018-08-23 12:21:25 -0400

    Completed. comments not complete

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
 #define TYPE_ONE_AMOUNT_SIZE 1
 #define TYPE_TWO_AMOUNT_SIZE 3
 #define MAX_NUMBER_SIZE 5
 
-
+/*
+  Read two bytes from file and
+  returns a 16 bit integer.
+  Increments the file pointer in the
+  process.
+*/
 uint16_t readTwoByteInt(FILE** file) {
 
 	char first;
@@ -18,56 +24,100 @@ uint16_t readTwoByteInt(FILE** file) {
 	return((uint16_t)first << 8) | (uint16_t)second;
 }
 
+/*
+ Reads three bytes from the file(incrementing the file pointer)
+ and converts the charactes read to a number.
+*/
 uint32_t readThreeByteInt(FILE** file)
 {
-	char number[TYPE_ONE_AMOUNT_SIZE];
-	fread(number, TYPE_TWO_AMOUNT_SIZE, 1, file);
+	char number[TYPE_TWO_AMOUNT_SIZE];
+	fread(number, TYPE_TWO_AMOUNT_SIZE, 1, *file);
 	return atoi(number);
 }
 
-char* readNumber(FILE** file)
+
+bool hasFinishedReadingNumber(char c) {
+	return c == ',';
+}
+
+bool hasFinishedReaddingAll(char c) {
+	return  c == '\x1'||
+		c == '\0';
+}
+char* readNumberAsChar(FILE** file)
 {
-	char number[MAX_NUMBER_SIZE];
-	char digit;
+	char character;
+	int bytesRead = fread(&character, sizeof(character), 1, *file);
+	if (bytesRead < sizeof(character) || hasFinishedReadingNumber(character)) {
+		return NULL;
+	}
+
+	char* number = malloc(sizeof(char) * MAX_NUMBER_SIZE);
 	int index = 0;
-	do {
-		fread(&(digit) + index++, sizeof(digit), 1, *file);
-	} while (digit != ",");
-	return atoi(number);
+	number[index++] = character;
+	
+	while (true){
+		bytesRead = fread(&character, sizeof(character), 1, *file);
+		if (bytesRead < sizeof(character)) {
+			return number;
+		}
+		if (hasFinishedReadingNumber(character)) {
+			break;
+		}
+		if (hasFinishedReaddingAll(character)) {
+			fseek(*file, -1, SEEK_CUR); 
+			/* Getting a char increments file pointer by 1.
+			   The char recieved tells us if we need to stop
+			   but we need to read that char again for the next iteration.
+				We need to decrement by 1 to be able to read again later.
+			*/
+			break;
+		}
+		number[index++] = character;
+	} 
+
+	return number;
 }
 
 int main() {
-
 	
-	char* fileName = "file1.bin";
+	char* fileName = "file2.bin";
 	FILE* file;
 	fopen_s(&file, fileName, "rb");
 
-	char g[50];
-	fread(g, sizeof(g), 1, file);
-
-	char type;
-	fread(&type, sizeof(type), 1, file);
-
-	if (type == 0) {
-		char amount;
-		fread(&amount, TYPE_ONE_AMOUNT_SIZE, 1, file);
-		printf("%d ", amount);
-		for (int i = 0; i < amount - 1; i++) {
+	while (true) {
+		char type;
+		int bytesRead = fread(&type, sizeof(type), 1, file);
+		if (bytesRead < sizeof(type)) {
+			break;
+		}
+		if (type == 0) {
+			char amount;
+			fread(&amount, TYPE_ONE_AMOUNT_SIZE, 1, file);
+			printf("%d ", amount);
+			for (int i = 0; i < amount - 1; i++) {
+				uint16_t number = readTwoByteInt(&file);
+				printf("%d ", number);
+			}
 			uint16_t number = readTwoByteInt(&file);
-			printf("%d," , number);
+			printf("%d\n", number);
 		}
-		uint16_t number = readTwoByteInt(&file);
-		printf("%d\n", number);
-	}
-	else {
-		uint32_t amount = readThreeByteInt(&file);
-		printf("%d ", amount);
-		for (int i = 0; i < amount - 1; i++) {
-			uint16_t number = readNumber(&file);
-			printf("%d, ", number);
+		else {
+			uint32_t amount = readThreeByteInt(&file);
+			printf("%d ", amount);
+			for (int i = 0; i < amount - 1; i++) {
+				char* number = readNumberAsChar(&file);
+				if (number != NULL) {
+					printf("%d, ", atoi(number));
+				}
+				free(number);
+			}
+			char* number = readNumberAsChar(&file);
+			if (number != NULL) {
+				printf("%d\n", atoi(number));
+				free(number);
+			}
 		}
-
 	}
 	fclose(file);
 
